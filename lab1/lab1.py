e3c48c4604de96efd9b25d56b25ca0fa53d391a3
katel <lutsai.k@gmail.com>
2022-09-28 14:05:12 +0200

    lab 1 comments

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -3,21 +3,24 @@ import numpy as np
 factors = 3
 points = 8
 max_lim = 20
-coefs = [1, 3, 3, 7]
+coefs = [1, 3, 3, 7]  # довільні
 
-
+# функція відгуку для однієї точки
+# повертає значення у точці
 def regression(factors, coefs):
     y = coefs[0] + coefs[1] * factors[0] + coefs[2] * factors[1] + coefs[3] * factors[2]
     return y
 
-
+# застосування функції відгуку до всієї матриці факторів
+# повертає масив значень для усіх точок
 def func_values(matrix):
     values = np.zeros((points, 1), matrix.dtype)
     for p in range(points):
         values[p] = regression(matrix[p], coefs)
     return values
 
-
+# нормалізація факторів
+# повертає матрицю нормалізованих факторів
 def norm_factors(matrix):
     xMin, xMax = matrix.min(0), matrix.max(0)
     x0 = (xMax + xMin)/2
@@ -28,16 +31,21 @@ def norm_factors(matrix):
         norm[p] = (matrix[p] - x0)/dx
     return norm
 
-
+# отримання індексу точки, що відповідає критерію
+# повертає елемент що має найменшу різницю з середнім значенням по модулю
 def point_by_crit(values):
     return np.abs(values-values.mean()).argmin()
 
+# генерація матриці факторів з випадкових чисел
 matrix = np.random.randint(0, max_lim, factors*points, int).reshape((points, factors))
 
+# запис матриці з нормалізованих факторів та їх значень за фунцією відгуку
 norm = norm_factors(matrix)
 norm = np.append(norm, func_values(norm), axis=1)
 
+# додаванн до матриці факторів колонки з їх значеннями
 matrix = np.append(matrix, func_values(matrix), axis=1)
+# індекс точки та її фактори, що задовільняють критерій вибору
 best_point = point_by_crit(matrix[:, -1])
 best_factors = matrix[best_point, :-1]
 
