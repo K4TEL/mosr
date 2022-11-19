c8017e32a17fd9b673a3db2937da71bcff460e79
klutsai <k.lutsai@viseven.com>
2022-11-19 15:17:18 +0100

    lab 5-6

--- lab6/lab6.py
+++ lab6/lab6.py
@@ -1,16 +1,20 @@
 import numpy as np
-from sklearn.preprocessing import minmax_scale
+import random
 
-x1_min, x1_max = -25, 75
-x2_min, x2_max = 25, 65
-x3_min, x3_max = 25, 40
+x1_min, x1_max = 10, 50
+x2_min, x2_max = -20, 60
+x3_min, x3_max = -20, 20
 
-y_min = 200 + (x1_min + x2_min + x3_min)/3
-y_max = 200 + (x1_max + x2_max + x3_max)/3
+x01 = (x1_min + x1_max)/2
+x02 = (x2_min + x2_max)/2
+x03 = (x3_min + x3_max)/2
 
-n = 8
+l = 1.73
+
+n = 14
 k = 3
-c = 8
+c = 11
+m = 2
 
 norm_factors = np.array([[1, 1, 1],
                          [1, 1, -1],
@@ -19,16 +23,38 @@ norm_factors = np.array([[1, 1, 1],
                          [-1, 1, 1],
                          [1, -1, 1],
                          [-1, -1, 1],
-                         [-1, 1, -1]])
+                         [-1, 1, -1],
+                         [-l, 0, 0],
+                         [l, 0, 0],
+                         [0, -l, 0],
+                         [0, l, 0],
+                         [0, 0, -l],
+                         [0, 0, l]])
+
+# генерація значень Y
+def generate_y(factors, n, m):
+    Y = np.zeros((n, m))
+    for r in range(n):
+        x1, x2, x3 = factors[r, 0:3]
+        for i in range(m):
+            Y[r, i] = 8.8 + 8.0 * x1 + 5.4 * x2 + 8.0 * x3 + 0.2 * x1 * x1 + \
+                      0.2 * x2 * x2 + 2.9 * x3 * x3 + 3.4 * x1 * x2 + 0.9 * \
+                      x1 * x3 + 3.5 * x2 * x3 + 0.3 * x1 * x2 * x3 + random.randint(0,10) - 5
+    return Y
+
 
 # інтеракції між факторами
-def interact_factors(factors):
-    full_factors = np.zeros((n, 4))
+def interact_factors(factors, expanded=False):
+    full_factors = np.zeros((n, 4)) if not expanded else np.zeros((n, 7))
     full_factors[:, 0] = factors[:, 0] * factors[:, 1]
     full_factors[:, 1] = factors[:, 0] * factors[:, 2]
     full_factors[:, 2] = factors[:, 1] * factors[:, 2]
     full_factors[:, 3] = factors[:, 0] * factors[:, 1] * factors[:, 2]
+    if expanded:
+        for i in range(3):
+            full_factors[:, 4+i] = np.power(factors[:, i], 2)
     return full_factors
+
 
 # отримання натуральних факторів з нормованих
 def natural(norm_factors):
@@ -36,10 +62,23 @@ def natural(norm_factors):
     natural[:, 0][norm_factors[:, 0] == 1] = x1_max
     natural[:, 1][norm_factors[:, 1] == 1] = x2_max
     natural[:, 2][norm_factors[:, 2] == 1] = x3_max
+
     natural[:, 0][norm_factors[:, 0] == -1] = x1_min
     natural[:, 1][norm_factors[:, 1] == -1] = x2_min
     natural[:, 2][norm_factors[:, 2] == -1] = x3_min
+
+    natural[:, 0][norm_factors[:, 0] == 0] = x01
+    natural[:, 1][norm_factors[:, 1] == 0] = x02
+    natural[:, 2][norm_factors[:, 2] == 0] = x03
+
+    natural[:, 0][norm_factors[:, 0] == l] = l * (x1_max - x01) + x01
+    natural[:, 1][norm_factors[:, 1] == l] = l * (x2_max - x02) + x02
+    natural[:, 2][norm_factors[:, 2] == l] = l * (x3_max - x03) + x03
+    natural[:, 0][norm_factors[:, 0] == -l] = -l * (x1_max - x01) + x01
+    natural[:, 1][norm_factors[:, 1] == -l] = -l * (x2_max - x02) + x02
+    natural[:, 2][norm_factors[:, 2] == -l] = -l * (x3_max - x03) + x03
     return natural
+
 
 # регресія за факторами та коефцієнтами
 def regression(matrix, coefs, n, c):
@@ -53,13 +92,6 @@ def regression(matrix, coefs, n, c):
         values[f] = func(matrix[f], coefs, c)
     return values
 
-# вирішення нормованого СЛР
-def solve_norm_coef(factors, values):
-    coefs = np.zeros(c)
-    coefs[0] = values.mean()
-    for i in range(c-1):
-        coefs[i+1] = np.mean(values * factors[:, 0])
-    return coefs
 
 # вирішення натурального СЛР
 def solve_coef(factors, values):
@@ -75,34 +107,28 @@ def solve_coef(factors, values):
     coef = np.linalg.solve(A, B)
     return coef
 
+
 print(f"X1 min: {x1_min}\tX1 max: {x1_max}")
 print(f"X2 min: {x2_min}\tX2 max: {x2_max}")
 print(f"X3 min: {x3_min}\tX3 max: {x3_max}")
-print(f"Y min: {y_min}\tY max: {y_max}")
 
+factors = natural(norm_factors)
+
+# Перевірка однорідності дисперсії за критерієм Кохрена
 # отримання оптимальної кількості випробувань для однієї комбінації факторів
-Gt = [6.798, 5.157, 4.737, 3.91]
-for i in range(2, 10):
-    rand_y = np.random.randint(y_min, y_max, (n, i))
+Gt = 0.3346
+for i in range(m, 10):
+    rand_y = generate_y(factors, n, i)
     std_y = np.std(rand_y, axis=1)**2
     print(f"Max Y dispersion: {std_y.max()}")
     Gp = std_y.max()/std_y.mean()
-    f1 = i - 1
-    print(f"m = {i} Gp: {Gp}\tGt: {Gt[i-2]}")
-    if Gp < Gt[i-2]:
+    print(f"m = {i} Gp: {Gp}\tGt: {Gt}")
+    if Gp < Gt:
         m = i
         break
 
 print(f"Mean Y dispersion: {std_y.mean()}")
-
-factors = natural(norm_factors)
-factors = np.concatenate((factors, interact_factors(factors)), axis=1)
-
-norm_factors = np.concatenate((norm_factors, interact_factors(norm_factors)), axis=1)
-
-# генерація Y значень для експериментів
-rand_y = np.random.randint(y_min, y_max, (n, m))
-norm_y = minmax_scale(rand_y.reshape(n*m), feature_range=(-1,1)).reshape(n ,m)
+factors = np.concatenate((factors, interact_factors(factors, True)), axis=1)
 
 # перевірка за критеріїм стюдента
 coefs_value = np.zeros(4)
@@ -110,33 +136,30 @@ coefs_value[0] = np.mean(rand_y.mean(axis=1))
 for i in range(3):
     coefs_value[i+1] = np.mean(rand_y.mean(axis=1) * factors[:, i])
 stud_crit = np.abs(coefs_value) / np.sqrt(std_y.mean()/(n*m))
-ts = [2.306, 2.12, 2.064]
-sig_coefs = len(stud_crit[stud_crit > ts[m-2]])
-print(f"All coefs are significant: {sig_coefs == k}\t{sig_coefs}")
+ts = 2.048
+sig_ind = np.argwhere(stud_crit > ts)
+
+print(f"All coefs are significant: {len(sig_ind.flatten()) == k}\t{sig_ind.flatten()}")
 
 # формування планів
-norm_plan = np.concatenate((norm_factors, norm_y), axis=1)
-norm_plan = np.concatenate((norm_plan, norm_y.mean(axis=1).reshape(n, 1)), axis=1)
-std_norm_y = np.std(norm_y, axis=1)**2
-norm_plan = np.concatenate((norm_plan, std_norm_y.reshape(n, 1)), axis=1)
-print(f"Normalized plan:\n{norm_plan}")
 plan = np.concatenate((factors, rand_y), axis=1)
 plan = np.concatenate((plan, rand_y.mean(axis=1).reshape(n, 1)), axis=1)
 plan = np.concatenate((plan, std_y.reshape(n, 1)), axis=1)
 print(f"Natural plan:\n{plan}")
 
-norm_coefs = solve_norm_coef(norm_factors, norm_y.mean(axis=1))
-print("Normalized coefs", norm_coefs)
 coefs = solve_coef(factors, rand_y.mean(axis=1))
 print("Natural coefs", coefs)
+significant_coefs = np.zeros_like(coefs)
+significant_coefs[sig_ind] = coefs[sig_ind]
+print("Significant coefs", significant_coefs)
 
-reg_val = regression(norm_factors, norm_coefs, n, k)
+reg_val = regression(factors, significant_coefs, n, k)
+print("Natural mean Y - regression Y")
+print(np.column_stack((rand_y.mean(axis=1), reg_val)))
 
 # перевірка критерію Фішера
-f3 = n * (m -1) # 8
-f4 = n - sig_coefs # 4
-disp = m/(n - sig_coefs) * np.sum((reg_val - norm_y.mean(axis=1))**2)
+disp = m/(n - len(sig_ind.flatten())) * np.sum((reg_val - rand_y.mean(axis=1))**2)
 Fp = disp / std_y.mean()
-Ft = 3.8
+Ft = 3.0
 print(f"Fisher crit: {Fp < Ft}\t{Fp}\t{Ft}")
 
